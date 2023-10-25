fc732a489aba3e9ef893027f1467d33710bf6b18
Tue Nguyen <diepanhng0711@gmail.com>
2023-10-25 12:49:56 +0700

    Use Debug.Log for console debug window

--- Assets/Scripts/TestConnect.cs
+++ Assets/Scripts/TestConnect.cs
@@ -9,7 +9,7 @@ public class TestConnect : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {   
-        print("Connecting to the server");
+        Debug.Log("Connecting to the server");
         PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
         PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
         PhotonNetwork.ConnectUsingSettings();
@@ -23,12 +23,12 @@ public class TestConnect : MonoBehaviourPunCallbacks
 
     // Connect to the master server
     public override void OnConnectedToMaster() {
-        print("Connecting to the server");
-        print(PhotonNetwork.LocalPlayer.NickName);
+        Debug.Log("Connecting to the server");
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName);
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        print("Disconnected from the server for the reason " + cause.ToString());
+        Debug.Log("Disconnected from the server for the reason " + cause.ToString());
     }
 }
